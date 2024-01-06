2e89db9053176e500a231516a984641fdc39fe3b
Velvet on Kimmy <velvet.aubry@mailo.com>
2024-01-06 15:57:20 +0100

    choix de technologie : tone.js

---
+++ index.js
@@ -0,0 +1,9 @@
+import * as Tone from "./node_modules/tone"
+
+const playButt = document.querySelector("#play-button");
+playButt.addEventListener("click", () => {
+  //create a synth and connect it to the main output (your speakers)
+  const synth = new Tone.Synth().toDestination();
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease("C4", "8n");
+});
