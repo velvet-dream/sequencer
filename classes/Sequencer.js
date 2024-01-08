a1c20f2ed1587c9ceca7f7374aa0683e7fc570c6
Velvet <velvet.aubry@mailo.com>
2024-01-08 13:59:27 +0100

    oops

--- classes/Sequencer.js
+++ classes/Sequencer.js
@@ -1,4 +1,9 @@
 export default class Sequencer {
   static lookahead = 25.0; // How frequently to call scheduling function (in milliseconds)
   static scheduleAheadTime = 0.1; // How far ahead to schedule audio (sec)
+  static BPM = 120;
+
+  throwBeat() {
+    
+  }
 }
